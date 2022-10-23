9a2d72553c87e62f4e2381afe7a09c3261c11a81
osawa-koki <osawa-koki@osawa.dev>
2022-10-23 13:09:28 +0000

    flickrから画像データを取得した。

--- app/download.py
+++ app/download.py
@@ -14,14 +14,25 @@ FLICKR_KEY = os.getenv("FLICKR_KEY")
 FLICKR_SECRET = os.getenv("FLICKR_SECRET")
 
 if DEBUG:
-  print("FLICKR_KEY -> {}".format(FLICKR_KEY))
-  print("FLICKR_SECRET -> {}".format(FLICKR_SECRET))
-  
+    print("FLICKR_KEY -> {}".format(FLICKR_KEY))
+    print("FLICKR_SECRET -> {}".format(FLICKR_SECRET))
+
 # 各種設定
 WAIT_TIME = 1
 ANIMAL_NAME = sys.argv[1]
 DOWNLOAD_TO = "./dataware/collected/animalai/{}".format(ANIMAL_NAME)
 
+# flickr処理
+flickr = FlickrAPI(FLICKR_KEY, FLICKR_SECRET, format="parsed-json")
+result = flickr.photos.search(
+    text = ANIMAL_NAME,
+    per_page = 400,
+    media = "photos",
+    sort = "relevance",
+    safe_search = 1,
+    extras = "url_q, license",
+)
 
-
+photos = result["photos"]
+pprint(photos)
 
